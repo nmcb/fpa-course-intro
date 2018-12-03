b89b0beb50a8b910a33f7913d16f3867819dd026
nmcb <marco@splatter.nl>
2018-12-03 16:15:21 +0100

    bump cats effect to 1.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val scalaTest     = "org.scalatest"  %% "scalatest"      % "3.0.5"
   lazy val scalaCheck    = "org.scalacheck" %% "scalacheck"     % "1.14.0"
   lazy val catsCore      = "org.typelevel"  %% "cats-core"      % "1.1.0"
-  lazy val catsEffect    = "org.typelevel"  %% "cats-effect"    % "0.10.1"
+  lazy val catsEffect    = "org.typelevel"  %% "cats-effect"    % "1.1.0"
   lazy val fastparse     = "com.lihaoyi"    %% "fastparse"      % "2.0.4"
   lazy val kindProjector = "org.spire-math" %% "kind-projector" % "0.9.7"
 }
