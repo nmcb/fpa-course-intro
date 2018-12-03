78cc46be17cedcfe4b4b7d91b8534732cd961480
nmcb <marco@splatter.nl>
2018-12-03 16:19:41 +0100

    bump cats core, fastparse and kind-projector to latest

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object Dependencies {
   lazy val scalaTest     = "org.scalatest"  %% "scalatest"      % "3.0.5"
   lazy val scalaCheck    = "org.scalacheck" %% "scalacheck"     % "1.14.0"
-  lazy val catsCore      = "org.typelevel"  %% "cats-core"      % "1.1.0"
+  lazy val catsCore      = "org.typelevel"  %% "cats-core"      % "1.5.0"
   lazy val catsEffect    = "org.typelevel"  %% "cats-effect"    % "1.1.0"
-  lazy val fastparse     = "com.lihaoyi"    %% "fastparse"      % "2.0.4"
-  lazy val kindProjector = "org.spire-math" %% "kind-projector" % "0.9.7"
+  lazy val fastparse     = "com.lihaoyi"    %% "fastparse"      % "2.0.5"
+  lazy val kindProjector = "org.spire-math" %% "kind-projector" % "0.9.8"
 }
