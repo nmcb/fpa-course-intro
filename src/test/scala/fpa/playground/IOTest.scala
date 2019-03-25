e38dba55374506ad53b1cfac24f2fa681ab20cca
nmcb <marco@splatter.nl>
2019-03-25 17:18:47 +0100

    add simple stack safety test

--- src/test/scala/fpa/playground/IOTest.scala
+++ src/test/scala/fpa/playground/IOTest.scala
@@ -11,18 +11,26 @@ class IOTest extends FlatSpec with Matchers {
     prog.unsafePerformIO should be(1)
   }
 
-  // Takes 3 min to finish, enable to validate stack safety
-  ignore should "be stack-safe" in {
+  it should "be stack-safe" in {
 
-    def ackermann(m: Int, n: Int): IO[Int] = (m, n) match {
-      case (0, n) => IO.delay(n + 1)
-      case (m, 0) => IO.suspend(ackermann(m - 1, 1))
-      case _      => for {
-        inner <- IO.suspend(ackermann(m, n - 1))
-        outer <- IO.suspend(ackermann(m - 1, inner))
-      } yield outer
-    }
+    def even(l: List[Int]): IO[Boolean] =
+      if (l.isEmpty) IO.delay(true) else IO.suspend(odd(l.tail))
 
-    ackermann(4, 1).unsafePerformIO should be (65533)
+    def odd(l: List[Int]): IO[Boolean] =
+      if (l.isEmpty) IO.delay(false) else IO.suspend(even(l.tail))
+
+    even((1 to 1000000).toList).unsafePerformIO should be (true)
+
+    /** Takes 3 min to finish, enable to validate continuation testing of stack safety */
+//    def ackermann(m: Int, n: Int): IO[Int] = (m, n) match {
+//      case (0, n) => IO.delay(n + 1)
+//      case (m, 0) => IO.suspend(ackermann(m - 1, 1))
+//      case _      => for {
+//        inner <- IO.suspend(ackermann(m, n - 1))
+//        outer <- IO.suspend(ackermann(m - 1, inner))
+//      } yield outer
+//    }
+//
+//    ackermann(4, 1).unsafePerformIO should be (65533)
   }
 }
