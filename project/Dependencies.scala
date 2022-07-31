00f9aa3a1ef012ac373e3bfbe4c8873d453ae084
nmc.borst <marco@splatter.nl>
2022-07-31 06:05:07 +0000

    Update scalatest to 3.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,6 @@ object Dependencies {
   lazy val catsCore      = "org.typelevel"  %% "cats-core"      % "2.8.0"
   lazy val catsEffect    = "org.typelevel"  %% "cats-effect"    % "3.3.13"
 
-  lazy val scalaTest     = "org.scalatest"  %% "scalatest"      % "3.2.12"
+  lazy val scalaTest     = "org.scalatest"  %% "scalatest"      % "3.2.13"
   lazy val scalaCheck    = "org.scalacheck" %% "scalacheck"     % "1.16.0"
 }
