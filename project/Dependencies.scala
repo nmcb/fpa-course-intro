a7fd5b15d29273ff4ff54b61fd4f550941ab51bc
nmc.borst <marco@splatter.nl>
2022-11-13 06:24:20 +0000

    Update cats-core to 2.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val kindProjector = "org.typelevel"  %% "kind-projector" % "0.13.2" cross CrossVersion.full
 
-  lazy val catsCore      = "org.typelevel"  %% "cats-core"      % "2.8.0"
+  lazy val catsCore      = "org.typelevel"  %% "cats-core"      % "2.9.0"
   lazy val catsEffect    = "org.typelevel"  %% "cats-effect"    % "3.3.14"
 
   lazy val scalaTest     = "org.scalatest"  %% "scalatest"      % "3.2.14"
