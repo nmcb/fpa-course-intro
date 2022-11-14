b63acfb39ca16ad0c343fc2a5751df4fd2d7775a
nmc.borst <marco@splatter.nl>
2022-11-14 06:26:29 +0000

    Update cats-effect to 3.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val kindProjector = "org.typelevel"  %% "kind-projector" % "0.13.2" cross CrossVersion.full
 
   lazy val catsCore      = "org.typelevel"  %% "cats-core"      % "2.8.0"
-  lazy val catsEffect    = "org.typelevel"  %% "cats-effect"    % "3.3.14"
+  lazy val catsEffect    = "org.typelevel"  %% "cats-effect"    % "3.4.0"
 
   lazy val scalaTest     = "org.scalatest"  %% "scalatest"      % "3.2.14"
   lazy val scalaCheck    = "org.scalacheck" %% "scalacheck"     % "1.17.0"
