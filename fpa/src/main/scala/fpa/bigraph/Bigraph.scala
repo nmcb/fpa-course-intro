01399facf1c0fec857d4ed178d1c5a2bc7a51690
nmcb <nmcb@xs4all.nl>
2026-06-17 13:44:02 +0200

    style Bigraph.scala

--- fpa/src/main/scala/fpa/bigraph/Bigraph.scala
+++ fpa/src/main/scala/fpa/bigraph/Bigraph.scala
@@ -1,7 +1,7 @@
 package fpa
 package bigraph
 
-object Bigraph {
+object Bigraph:
 
   // dependencies
 
@@ -14,10 +14,9 @@ object Bigraph {
   case class Bar(s: String) extends Foo
   case class Baz(i: Int) extends Foo
 
-  class Disjoint[A, B, C](val ac: A <:< C, val bc: A <:< C) {
+  class Disjoint[A, B, C](val ac: A <:< C, val bc: A <:< C):
     def union(as: Set[A], bs: Set[B]): Set[C] =
       as.asInstanceOf[Set[C]].union(bs.asInstanceOf[Set[C]])
-  }
 
   implicit val fooDisjoint: Disjoint[Bar, Baz, Foo] =
     new Disjoint[Bar, Baz, Foo](implicitly, implicitly)
@@ -29,29 +28,21 @@ object Bigraph {
 
     // categorical
 
-  trait Obj[F[_]] {
+  trait Obj[F[_]]:
     def identity: Arr[F, F]
-  }
-  trait Arr[F[_], G[_]] {
+  
+  trait Arr[F[_], G[_]]:
     def compose[H[_]](lhs: Arr[F, G])(rhs: Arr[G, H]): Arr[F, H]
     def dom: Obj[F]
     def cod: Obj[G]
-  }
 
-  implicit val setObj: Obj[Set] = new Obj[Set] {
-    override def identity: Arr[Set, Set] = ???
-  }
+  implicit val objSet: Obj[Set] = new Obj[Set]:
+    override def identity: Arr[Set, Set] =
+      sys.error("unimplemented")
 
-  trait Functor[F[_]] {
+  trait Functor[F[_]]:
     def map[A, B](fa: F[A])(f: A => B): F[B]
-  }
 
-  implicit def setFunctor[A, B]: Functor[Set] = new Functor[Set] {
+  implicit def functorSet: Functor[Set] = new Functor[Set]:
     override def map[A, B](fa: Set[A])(f: A => B): Set[B] =
       fa.map(f)
-  }
-
-  object examples {
-
-  }
-}
