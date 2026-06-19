b3a4fc8dc3b0b146f169c4ee129a815980ac5ba1
nmcb <nmcb@xs4all.nl>
2026-06-19 10:15:51 +0200

    fix style fps

--- fps/src/main/scala/fps/fps-nmcb.scala
+++ fps/src/main/scala/fps/fps-nmcb.scala
@@ -12,14 +12,15 @@ case object Non          extends Optional[Nothing]
 case class  The[A](a: A) extends Optional[A]
 
 object Optional:
-  implicit val optionalFunctorial: Functorial[Optional] =
+  
+  given Functorial[Optional] =
     new Functorial[Optional]:
       def map[A,B](f: A => B)(oi: Optional[A]): Optional[B] =
         oi match
           case Non    => Non
           case The(a) => The(f(a))
 
-  implicit class OptionalOps[A](oa: Optional[A])(implicit F : Functorial[Optional]):
+  extension [A](oa: Optional[A])(using F : Functorial[Optional])
     def map[B](f: A => B): Optional[B] =
       F.map(f)(oa)
 
@@ -35,14 +36,14 @@ object Multiple:
   def apply[A](as: A*): Multiple[A] =
     as.foldRight(Multiple.empty)((elm,acc) => Cell(elm, acc))
 
-  implicit val multipleFunctorial: Functorial[Multiple] =
+  given Functorial[Multiple] =
     new Functorial[Multiple]:
       def map[A,B](f: A => B)(oi: Multiple[A]): Multiple[B] =
         oi match
           case Empty      => Empty
           case Cell(a, r) => Cell(f(a), map(f)(r))
 
-  implicit class MultipleOps[A](ma: Multiple[A])(implicit F : Functorial[Multiple]):
+  extension [A](ma: Multiple[A])(using F : Functorial[Multiple])
     def map[B](f: A => B): Multiple[B] =
       F.map(f)(ma)
 
